4f16f0e2837da9b4aa1d44c1cd93c97d918829ff
JonnyD <contact@jonnydevine.com>
2013-03-03 20:29:49 +0000

    Added logger

--- src/civchat/CivChat.java
+++ src/civchat/CivChat.java
@@ -1,18 +1,22 @@
 package civchat;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class CivChat extends JavaPlugin
 {
+	private static Logger log;
 	
 	public void onEnable() 
 	{
-		
+		log = this.getLogger();
+		log.info("CivChat has been enabled.");
 	}
 	
 	public void onDisable()
 	{
-		
+		log.info("CivChat has been disabled");
 	}
 }
