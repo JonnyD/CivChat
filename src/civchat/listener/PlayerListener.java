4a9a2e5644f73618f8ba0defa12001a22affa4d9
JonnyD <contact@jonnydevine.com>
2013-03-03 23:37:14 +0000

    Added login, logout, interact events

--- src/civchat/listener/PlayerListener.java
+++ src/civchat/listener/PlayerListener.java
@@ -3,6 +3,8 @@ package civchat.listener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import civchat.manager.PlayerManager;
 import civchat.model.CivPlayer;
@@ -10,6 +12,28 @@ import civchat.model.Mode;
 
 public class PlayerListener implements Listener
 {
+	public void login(PlayerLoginEvent event)
+	{
+		Player player = event.getPlayer();
+		
+		PlayerManager playerManager = PlayerManager.getInstance();
+		CivPlayer civPlayer = playerManager.getCivPlayer(player);
+		
+		if(civPlayer == null)
+		{
+			civPlayer = new CivPlayer(player);
+			playerManager.addCivPlayer(civPlayer);
+		}
+	}
+	
+	public void quit(PlayerQuitEvent event)
+	{
+		Player player = event.getPlayer();
+		
+		PlayerManager playerManager = PlayerManager.getInstance();
+		playerManager.removeCivPlayer(player);
+	}
+	
 	public void interact(PlayerInteractEvent event)
 	{
 		if(!event.hasBlock())
@@ -23,18 +47,16 @@ public class PlayerListener implements Listener
 		PlayerManager playerManager = PlayerManager.getInstance();
 		CivPlayer civPlayer = playerManager.getCivPlayer(playerName);
 		
-		if(civPlayer == null)
+		if(civPlayer != null)
 		{
-			return;
-		}
-		
-		Mode mode = civPlayer.getMode();
-		switch(mode)
-		{
-			case NORMAL:
-				return;
-			case CREATE_ANTENNA:
-				System.out.println("created antenna!");
+			Mode mode = civPlayer.getMode();
+			switch(mode)
+			{
+				case NORMAL:
+					return;
+				case CREATE_ANTENNA:
+					System.out.println("created antenna!");
+			}
 		}
 	}
 }
