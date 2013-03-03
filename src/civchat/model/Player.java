9a2259c3ae30ec82c1ab7a797de84dc90306a9a7
JonnyD <contact@jonnydevine.com>
2013-03-03 21:58:04 +0000

    Added model Player

---
+++ src/civchat/model/Player.java
@@ -0,0 +1,19 @@
+package civchat.model;
+
+public class Player
+{
+
+	private Mode mode;
+	
+	public Player(){}
+	
+	public Mode getMode()
+	{
+		return this.mode;
+	}
+	
+	public void setMode(Mode mode)
+	{
+		this.mode = mode;
+	}
+}
