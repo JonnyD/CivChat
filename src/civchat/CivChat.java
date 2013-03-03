f6ca055d8d7b4b3d50b05ba8ba61c00a2ac4acea
JonnyD <contact@jonnydevine.com>
2013-03-03 20:26:36 +0000

    Initial Commit

---
+++ src/civchat/CivChat.java
@@ -0,0 +1,18 @@
+package civchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class CivChat extends JavaPlugin
+{
+	
+	public void onEnable() 
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+}
