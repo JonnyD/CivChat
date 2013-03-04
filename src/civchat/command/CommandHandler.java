e5851edfd8952c59485115d6c24b056ef96e94c2
JonnyD <contact@jonnydevine.com>
2013-03-04 13:55:35 +0000

    Added Command help for when arguments length doesn't meet the required amount

--- src/civchat/command/CommandHandler.java
+++ src/civchat/command/CommandHandler.java
@@ -24,11 +24,17 @@ public class CommandHandler
 			if(args.length < command.getMinArguments() 
 			|| args.length > command.getMaxArguments())
 			{
-				//displayhelp
-				return true;
+				this.displayCommandHelp(command, sender);
 			}
 			command.execute(sender, args);
 		}
-		return true;
+		return false;
+	}
+	
+	public void displayCommandHelp(Command command, CommandSender sender)
+	{
+		sender.sendMessage(new StringBuilder().append("§cCommand:§e ").append(command.getName()).toString());
+		sender.sendMessage(new StringBuilder().append("§cDescription:§e ").append(command.getDescription()).toString());
+		sender.sendMessage(new StringBuilder().append("§cUsage:§e ").append(command.getUsage()).toString());   
 	}
 }
