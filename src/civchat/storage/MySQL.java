b098029af6680dff583ee5dc0332f80a1ad21751
JonnyD <contact@jonnydevine.com>
2013-03-04 22:15:39 +0000

    Changed to EbeanServer

---
+++ src/civchat/storage/MySQL.java
@@ -0,0 +1,46 @@
+package civchat.storage;
+
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import civchat.CivChat;
+import civchat.manager.ConfigManager;
+
+public class MySQL extends DB
+{	
+	private CivChat plugin;
+	public MySQL() {
+        super(CivChat.getInstance());
+        
+        plugin = CivChat.getInstance();
+        
+        ConfigManager configManager = plugin.getConfigManager();
+        String driver = configManager.getDriver();
+        String url = configManager.getUrl();
+        String username = configManager.getUsername();
+        String password = configManager.getPassword();
+        String isolation = configManager.getIsolation();
+        boolean logging = configManager.isLogging();
+        boolean rebuild = configManager.isRebuild();
+        
+        super.initializeDatabase(
+        		driver, url, username, password, isolation, logging, rebuild
+        );
+    }
+
+    @Override
+    protected List<Class<?>> getDatabaseClasses() {
+        return Arrays.asList();
+    }
+
+    public Object save(Object object) {
+        getDatabase().save(object);
+        return object;
+    }
+
+    public void delete(Object object) {
+        getDatabase().delete(object);
+    }
+}
