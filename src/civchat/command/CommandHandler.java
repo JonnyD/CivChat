d225f6e0fc6f2ba881b4249cfca18b8087ca831e
JonnyD <contact@jonnydevine.com>
2013-03-04 13:58:39 +0000

    Formatted code

--- src/civchat/command/CommandHandler.java
+++ src/civchat/command/CommandHandler.java
@@ -7,22 +7,21 @@ import org.bukkit.command.CommandSender;
 
 public class CommandHandler 
 {
-
 	private Map<String, Command> commands = new LinkedHashMap<String, Command>();
-	
-	public void addCommand(Command command)
+
+	public void addCommand(Command command) 
 	{
 		String identifier = command.getIdentifier().toLowerCase();
 		this.commands.put(identifier, command);
 	}
-	
-	public boolean dispatch(CommandSender sender, String label, String[] args)
+
+	public boolean dispatch(CommandSender sender, String label, String[] args) 
 	{
-		if(commands.containsKey(label))
+		if (commands.containsKey(label)) 
 		{
 			Command command = commands.get(label);
-			if(args.length < command.getMinArguments() 
-			|| args.length > command.getMaxArguments())
+			if (args.length < command.getMinArguments()
+					|| args.length > command.getMaxArguments()) 
 			{
 				this.displayCommandHelp(command, sender);
 			}
@@ -30,11 +29,14 @@ public class CommandHandler
 		}
 		return false;
 	}
-	
-	public void displayCommandHelp(Command command, CommandSender sender)
+
+	public void displayCommandHelp(Command command, CommandSender sender) 
 	{
-		sender.sendMessage(new StringBuilder().append("§cCommand:§e ").append(command.getName()).toString());
-		sender.sendMessage(new StringBuilder().append("§cDescription:§e ").append(command.getDescription()).toString());
-		sender.sendMessage(new StringBuilder().append("§cUsage:§e ").append(command.getUsage()).toString());   
+		sender.sendMessage(new StringBuilder().append("§cCommand:§e ")
+				.append(command.getName()).toString());
+		sender.sendMessage(new StringBuilder().append("§cDescription:§e ")
+				.append(command.getDescription()).toString());
+		sender.sendMessage(new StringBuilder().append("§cUsage:§e ")
+				.append(command.getUsage()).toString());
 	}
 }
