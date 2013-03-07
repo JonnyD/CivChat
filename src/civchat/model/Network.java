5a88e9b11be53aba68b456d1f1e9b9518624d7d5
JonnyD <contact@jonnydevine.com>
2013-03-07 01:45:42 +0000

    Updated Network to allow for dirty field checking when updating entry in database

--- src/civchat/model/Network.java
+++ src/civchat/model/Network.java
@@ -1,13 +1,23 @@
 package civchat.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Network 
 {
 	private String name;
 	private String owner;
 	
+	private Set<DirtyNetworkReason> dirty = new HashSet<DirtyNetworkReason>();
+	public enum DirtyNetworkReason
+	{
+		NAME,
+		OWNER
+	}
+	
 	public Network(String name, String owner)
 	{
-		this.name = name;
+		this.name  = name;
 		this.owner = owner;
 	}
 
@@ -19,6 +29,7 @@ public class Network
 	public void setName(String name) 
 	{
 		this.name = name;
+		this.dirty.add(DirtyNetworkReason.NAME);
 	}
 
 	public String getOwner() {
@@ -27,7 +38,21 @@ public class Network
 
 	public void setOwner(String owner) {
 		this.owner = owner;
+		this.dirty.add(DirtyNetworkReason.OWNER);
 	}
 	
+	public boolean isDirty(DirtyNetworkReason dirtyType)
+	{
+		return this.dirty.contains(dirtyType);
+	}
 	
+	public boolean isDirty()
+	{
+		return !this.dirty.isEmpty();
+	}
+	
+	public void clearDirty()
+	{
+		this.dirty.clear();
+	}
 }
