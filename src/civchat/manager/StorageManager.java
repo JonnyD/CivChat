21e065a61a65b0ddf2f412bfc1083d28da20b256
JonnyD <contact@jonnydevine.com>
2013-03-07 22:48:38 +0000

    Cast Location coordinates to int

--- src/civchat/manager/StorageManager.java
+++ src/civchat/manager/StorageManager.java
@@ -20,16 +20,16 @@ public class StorageManager
 	private CivChat plugin;
 	private ConfigManager configManager;
 	private Logger log;
-	
+
 	public StorageManager()
 	{
 		plugin        = CivChat.getInstance();
 		configManager = plugin.getConfigManager();
 		log           = CivChat.getLog();
-		
+
 		initiateDB();
 	}
-	
+
 	private void initiateDB()
 	{
 		String host     = configManager.getHost();
@@ -37,20 +37,20 @@ public class StorageManager
 		String username = configManager.getUsername();
 		String password = configManager.getPassword();
 		int port        = configManager.getPort();
-		
+
 		db = new MySQL(host, port, database, username, password);
-		
+
 		if(db.checkConnection())
 		{
 			log.info("dbMysqlConnected");
-			
+
 			if(!db.existsTable("cc_network"))
 			{
 				log.info("Creating table: cc_network");
 				String query = "CREATE TABLE IF NOT EXISTS `cc_network` (`id` integer NOT NULL auto_increment, `name` varchar(100), `owner` varchar(100), PRIMARY KEY(`id`));";
 				db.execute(query);
 			}
-			
+
 			if(!db.existsTable("cc_antenna"))
 			{
 				log.info("Creating table: cc_antenna");
@@ -59,49 +59,49 @@ public class StorageManager
 			}
 		}
 	}
-	
+
 	public void insertAntenna(Antenna antenna)
 	{
 		String query  = "INSERT INTO `cc_antenna` (`x`, `y`, `z`, `owner`)";
 		String values = "VALUES (" + antenna.getX() + ", " + antenna.getY() + ", " + antenna.getZ() + ", '" + antenna.getOwner() + "')";	
-	
+
 		synchronized (this)
 		{
 			db.insert(query + values);
 		}
 	}
-	
+
 	public void updateAntenna(Antenna antenna)
 	{
 		String subQuery = "";
-		
+
 		if(antenna.isDirty(DirtyAntennaReason.OWNER))
 		{
 			subQuery += "owner = " + antenna.getOwner() + ", ";
 		}
 		if(antenna.isDirty(DirtyAntennaReason.NETWORK))
 		{
-			subQuery += "network = " + antenna.getNetwork() + ", ";
+			subQuery += "network_id = " + antenna.getNetworkId() + ", ";
 		}
 		if(antenna.isDirty(DirtyAntennaReason.DAMAGED))
 		{
 			subQuery += "damaged = " + antenna.isDamaged();
 		}
 
-        if (!subQuery.isEmpty())
-        {
-            String query = "UPDATE `cc_antenna` SET " + subQuery + " WHERE x = " + antenna.getX() + " AND y = " + antenna.getY() + " AND z = " + antenna.getZ() + " AND owner = '" + antenna.getOwner() + "';";
-            db.execute(query);
-        }
+		if (!subQuery.isEmpty())
+		{
+			String query = "UPDATE `cc_antenna` SET " + subQuery + " WHERE x = " + antenna.getX() + " AND y = " + antenna.getY() + " AND z = " + antenna.getZ() + " AND owner = '" + antenna.getOwner() + "';";
+			db.execute(query);
+		}
 
-        antenna.clearDirty();
+		antenna.clearDirty();
 	}
-	
+
 	public Antenna findAntenna(Location location)
 	{
 		Antenna antenna = null;
-		
-		String query  = "SELECT * FROM `cc_antenna`";
+
+		String query  = "SELECT * FROM `cc_antenna` WHERE x = " + (int) location.getX() + ", y = " + (int) location.getY() + ", z = " + (int) location.getY();
 		ResultSet res = db.select(query);
 		if(res != null)
 		{
@@ -116,7 +116,8 @@ public class StorageManager
 						int z           = res.getInt("z");
 						String owner    = res.getString("owner");
 						boolean damaged = res.getBoolean("damaged");
-						
+						int networkId   = res.getInt("network_id");
+
 						antenna = new Antenna(id, x, y, z, owner, damaged);
 					}
 					catch (Exception ex)
@@ -130,43 +131,43 @@ public class StorageManager
 				log.severe(ex.getMessage());
 			}
 		}
-		
+
 		return antenna;
 	}
-	
+
 	public void insertNetwork(Network network)
 	{
 		String query  = "INSERT INTO `cc_network` (`name`, `owner`)";
 		String values = "VALUES ('" + network.getName() + "', '" + network.getOwner() + "')";
-		
+
 		synchronized (this)
 		{
 			db.insert(query + values);
 		}
 	}
-	
+
 	public void updateNetwork(Network network)
 	{
 		String subQuery = "";
-		
+
 		if(network.isDirty(DirtyNetworkReason.OWNER))
 		{
 			subQuery += "owner = " + network.getOwner();
 		}
-		
+
 		if(!subQuery.isEmpty())
 		{
 			String query = "UPDATE `cc_network` SET " + subQuery + " WHERE name = " + network.getName();
 			db.execute(query);
 		}
-		
+
 		network.clearDirty();
 	}
-	
+
 	public Network findNetwork(String n)
 	{
 		Network network = null;
-		
+
 		String query  = "SELECT * FROM `cc_network` WHERE name = '" + n + "'";
 		ResultSet res = db.select(query);
 		if(res != null)
@@ -175,11 +176,12 @@ public class StorageManager
 			{
 				while(res.next())
 				{
-					try {
+					try 
+					{
 						int id       = res.getInt("id");
 						String name  = res.getString("name");
 						String owner = res.getString("owner");
-						
+
 						network = new Network(id, name, owner);
 					}
 					catch (Exception ex)
@@ -193,7 +195,7 @@ public class StorageManager
 				log.severe(ex.getMessage());
 			}
 		}
-				
+
 		return network;
 	}
 }
