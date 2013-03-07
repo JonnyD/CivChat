e3289b08ce99bf8186bd5ec9c3945dad1970f33f
JonnyD <contact@jonnydevine.com>
2013-03-07 16:49:20 +0000

    Added method to find location of antenna

--- src/civchat/manager/AntennaManager.java
+++ src/civchat/manager/AntennaManager.java
@@ -1,9 +1,14 @@
 package civchat.manager;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -25,9 +30,13 @@ public class AntennaManager
 		this.configManager  = plugin.getConfigManager();
 	}
 	
-	public void recordAntennaPlace(Player player, Block block)
+	public void recordAntennaPlace(Player player, Location location)
 	{
-		Antenna antenna = new Antenna(block.getX(), block.getY(), block.getZ(), player.getDisplayName());
+		int x             = (int) location.getX();
+		int y             = (int) location.getY();
+		int z             = (int) location.getZ();
+		String playerName = player.getDisplayName();
+		Antenna antenna   = new Antenna(x, y, z, playerName);
 		storageManager.insertAntenna(antenna);
 	}
 	
@@ -39,24 +48,78 @@ public class AntennaManager
 	
 	public boolean canBeAntenna(Block block) 
 	{
-		Set<Material> antennaMaterials = configManager.getAntennaMaterials();		
-		String antennaMaterialString = "";
-		for(Material m : antennaMaterials)
+		Set<Material> antennaMaterials = configManager.getAntennaMaterials();
+		if(antennaMaterials.contains(block.getType()))
 		{
-			antennaMaterialString += m.toString();
+			Material main  = block.getType();
+			Material up1   = block.getRelative(BlockFace.UP, 1).getType();
+			Material up2   = block.getRelative(BlockFace.UP, 2).getType();
+			Material down1 = block.getRelative(BlockFace.DOWN, 1).getType();
+			Material down2 = block.getRelative(BlockFace.DOWN, 2).getType();
+	
+			String materials = first3Letters(up1.toString()) + firstNumber(Integer.toString(up1.getId()))
+					+ first3Letters(up2.toString()) + firstNumber(Integer.toString(up2.getId()))
+					+ first3Letters(main.toString()) + firstNumber(Integer.toString(main.getId()))
+					+ first3Letters(down1.toString()) + firstNumber(Integer.toString(down1.getId()))
+					+ first3Letters(down2.toString()) + firstNumber(Integer.toString(down2.getId()));
+			
+			String regex = "DIA5IRO1JUK8........|IRO1....DIA5JUK8....|........IRO1DIA5JUK8";
+	
+			if (materials.matches(regex)) 
+			{
+				return true;
+			}
 		}
+		return false;
+	}
+	
+	public Location findLocation(Block block)
+	{
+		List<Material> antennaMaterials = new ArrayList<Material>(configManager.getAntennaMaterials());
+		Material material = antennaMaterials.get(0);
+
+		Block main = block;
+		Block down1 = block.getRelative(BlockFace.DOWN, 1);
+		Block down2 = block.getRelative(BlockFace.DOWN, 2);
 		
-		String incomingMaterialString = block.getType().toString();
-		for(int i = 1; i <= antennaMaterials.size(); i++)
+		Location location = null;
+		if(main.getType() == material)
 		{
-			Material above = block.getRelative(BlockFace.UP, i).getType();
-			incomingMaterialString += above.toString();
+			location = new Location(main.getWorld(), main.getX(), main.getY(), main.getZ());
+		}
+		else if(down1.getType() == material)
+		{
+			location = new Location(main.getWorld(), main.getX(), main.getY() - 1, main.getZ());
+		}
+		else if(down2.getType() == material)
+		{
+			location = new Location(main.getWorld(), main.getX(), main.getY() - 2, main.getZ());
 		}
 
-		if (antennaMaterialString.matches(incomingMaterialString)) {
+		return location;
+	}
+	
+	public boolean isAntenna(Location location)
+	{
+		if(getAntenna(location) != null)
+		{
 			return true;
 		}
-
 		return false;
+	}
+	
+	public Antenna getAntenna(Location location)
+	{
+		return null;
+	}
+	
+	private String first3Letters(String text)
+	{
+		return text.substring(0, 3);
+	}
+	
+	private String firstNumber(String text)
+	{
+		return text.substring(0, 1);
 	}
 }
