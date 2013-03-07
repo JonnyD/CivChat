2a49edfb5d71a48a7c3e6dcb621ab97ab920dd96
JonnyD <contact@jonnydevine.com>
2013-03-07 13:59:22 +0000

    Moved canBeAntenna from Utility to AntennaManager

--- src/civchat/manager/AntennaManager.java
+++ src/civchat/manager/AntennaManager.java
@@ -1,6 +1,11 @@
 package civchat.manager;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
 import civchat.CivChat;
@@ -11,11 +16,13 @@ public class AntennaManager
 {
 	private CivChat plugin;
 	private StorageManager storageManager;
+	private ConfigManager configManager;
 	
 	public AntennaManager()
 	{
-		this.plugin = CivChat.getInstance();
+		this.plugin         = CivChat.getInstance();
 		this.storageManager = plugin.getStorageManager();
+		this.configManager  = plugin.getConfigManager();
 	}
 	
 	public void recordAntennaPlace(Player player, Block block)
@@ -28,5 +35,28 @@ public class AntennaManager
 	{
 		antenna.setNetwork(civPlayer.getNetwork());
 		storageManager.updateAntenna(antenna);
+	}
+	
+	public boolean canBeAntenna(Block block) 
+	{
+		Set<Material> antennaMaterials = configManager.getAntennaMaterials();		
+		String antennaMaterialString = "";
+		for(Material m : antennaMaterials)
+		{
+			antennaMaterialString += m.toString();
+		}
+		
+		String incomingMaterialString = block.getType().toString();
+		for(int i = 1; i <= antennaMaterials.size(); i++)
+		{
+			Material above = block.getRelative(BlockFace.UP, i).getType();
+			incomingMaterialString += above.toString();
+		}
+
+		if (antennaMaterialString.matches(incomingMaterialString)) {
+			return true;
+		}
+
+		return false;
 	}
 }
