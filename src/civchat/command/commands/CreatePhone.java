718ef077c4056ede18386edc513dc50841ba862a
JonnyD <contact@jonnydevine.com>
2013-03-12 23:14:14 +0000

    Removed argument to set the network from this command and moved it to another command called Connect. It doesn't need to be set immediately and the player to scan for available networks and connect whenever they want.

--- src/civchat/command/commands/CreatePhone.java
+++ src/civchat/command/commands/CreatePhone.java
@@ -18,8 +18,8 @@ public class CreatePhone extends PlayerCommand
 	{
 		super("Create Phone");
 		setDescription("Creates a phone");
-		setUsage("/createphone <network>");
-		setArgumentRange(1,1);
+		setUsage("/createphone");
+		setArgumentRange(0,0);
 		setIdentifier("createphone");
 	}
 
@@ -27,38 +27,8 @@ public class CreatePhone extends PlayerCommand
 	public boolean execute(CommandSender sender, String[] args) 
 	{
 		Player player = (Player) sender;
-		
-		ItemStack itemInHand    = player.getItemInHand();
-		Material materialInHand = itemInHand.getData().getItemType();
-		
-		if(materialInHand != Material.COMPASS)
-		{
-			System.out.println("not a phone");
-			return true;
-		}
-		
-		String networkName            = args[0];
-		NetworkManager networkManager = plugin.getNetworkManager();
-		Network network               = networkManager.getNetwork(networkName);
-		
-		if(network == null)
-		{
-			System.out.println("network doesnt exist");
-			return true;
-		}
-		
 		MobilePhoneManager phoneManager = plugin.getMobilePhoneManager();
-		MobilePhone mobilePhone         = phoneManager.getMobilePhone(itemInHand.getDurability());
-		
-		if(mobilePhone != null)
-		{
-			System.out.println("Already a mobile phone");
-			return true;
-		}
-		
-		int phoneId = phoneManager.recordMobilePhone(network, player);
-		itemInHand.setDurability((short) phoneId);
-		
+		phoneManager.createPhone(player);
 		return true;
 	}
 	
