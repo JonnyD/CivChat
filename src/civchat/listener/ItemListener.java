6313a957a850a91c96ea3c6680690cdc9f38c436
JonnyD <contact@jonnydevine.com>
2013-03-12 23:36:00 +0000

    Added onEntityCombustEvent to delete mobile phones when they combust in lava or fires

--- src/civchat/listener/ItemListener.java
+++ src/civchat/listener/ItemListener.java
@@ -1,9 +1,11 @@
 package civchat.listener;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.ItemDespawnEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.Inventory;
@@ -23,6 +25,14 @@ public class ItemListener implements Listener
 		Player player       = event.getPlayer();
 		Inventory inventory = player.getInventory();
 		ItemStack item      = inventory.getItem(event.getNewSlot());
+		Material material   = item.getData().getItemType();
+		
+		if(material != Material.COMPASS)
+			return;
+		
+		int id = item.getDurability();
+		if(id == 0)
+			return;
 		
 		MobilePhoneManager phoneManager = plugin.getMobilePhoneManager();
 		phoneManager.announcePhone(player, item);
@@ -32,8 +42,34 @@ public class ItemListener implements Listener
 	public void onItemDespawnEvent(ItemDespawnEvent event)
 	{
 		ItemStack itemStack = event.getEntity().getItemStack();
+		Material material = itemStack.getData().getItemType();
+		if(material != Material.COMPASS)
+			return;
+		
 		int id = itemStack.getDurability();
+		if(id == 0)
+			return;
+		
 		MobilePhoneManager phoneManager = plugin.getMobilePhoneManager();
 		phoneManager.deleteMobilePhone(id);		
+	}
+	
+	@EventHandler
+	public void onEntityCombustEvent(EntityCombustEvent event)
+	{
+		if(!(event.getEntity() instanceof Item))
+			return;
+		
+		ItemStack itemStack = ((Item) event.getEntity()).getItemStack();
+		Material material = itemStack.getData().getItemType();
+		if(material != Material.COMPASS)
+			return;
+		
+		int id = itemStack.getDurability();
+		if(id == 0)
+			return;
+		
+		MobilePhoneManager phoneManager = plugin.getMobilePhoneManager();
+		phoneManager.deleteMobilePhone(id);	
 	}
 }
